610958b52cdc4b5dc8344f435b9a060d7e158a7b
Miquel Martín <mmgadmin@MacMiquel.local>
2015-09-16 15:06:12 +0200

    Prepare for 'Generate map' and 'Keep license note' preferences

--- prefs/preferences.js
+++ prefs/preferences.js
@@ -17,6 +17,8 @@ define(function(require, exports, module) {
     prefs.definePreference("js-mangle", "boolean", true);
     prefs.definePreference("js-project-minify", "boolean", true);
     prefs.definePreference("css-project-minify", "boolean", true);
+    prefs.definePreference("generate-map", "boolean", false);
+    prefs.definePreference("keep-license", "boolean", true);
 
     function getPref(pref, passedScope) {
         if (passedScope === "project" || passedScope === "user") {
@@ -43,7 +45,7 @@ define(function(require, exports, module) {
     }
 
     function showMinifierPreferencesDialog() {
-        var dialogWindow, projectSave, minifyJS, minifyCSS, jsPath, cssPath, excludes, mangleJS, compressJS;
+        var dialogWindow, projectSave, minifyJS, minifyCSS, generateMap, keepLicense, jsPath, cssPath, excludes, mangleJS, compressJS;
         $.valHooks.textarea = {
             get: function(elem) {
                 return elem.value.replace(/\r?\n/g, "<br>");
@@ -54,6 +56,8 @@ define(function(require, exports, module) {
                 setPref("on-save-project", projectSave[0].checked, "project");
                 setPref("js-project-minify", minifyJS[0].checked, "project");
                 setPref("css-project-minify", minifyCSS[0].checked, "project");
+                setPref("generate-map", generateMap[0].checked, "project");
+                setPref("keep-license", keepLicense[0].checked, "project");
                 setPref("js-custom-path", $.trim(jsPath.val()), "project");
                 setPref("css-custom-path", $.trim(cssPath.val()), "project");
                 setPref("project-exclude", $.trim(excludes.val()), "project");
@@ -67,6 +71,8 @@ define(function(require, exports, module) {
         projectSave = dialogWindow.find("#minifier-save-project");
         minifyJS    = dialogWindow.find("#minifier-js-bool");
         minifyCSS   = dialogWindow.find("#minifier-css-bool");
+        generateMap = dialogWindow.find("#minifier-generate-map-bool");
+        keepLicense = dialogWindow.find("#minifier-keep-license-bool");
         jsPath      = dialogWindow.find("#minifier-js-path");
         cssPath     = dialogWindow.find("#minifier-css-path");
         excludes    = dialogWindow.find("#minifier-exclude-paths");
@@ -76,6 +82,8 @@ define(function(require, exports, module) {
         projectSave[0].checked = !!getPref("on-save-project");
         minifyJS[0].checked    = !!getPref("js-project-minify");
         minifyCSS[0].checked   = !!getPref("css-project-minify");
+        generateMap[0].checked = !!getPref("generate-map");
+        keepLicense[0].checked = !!getPref("keep-license");
         mangleJS[0].checked    = !!getPref("js-mangle");
         compressJS[0].checked  = !!getPref("js-compress");
         jsPath.val(getPref("js-custom-path"));
